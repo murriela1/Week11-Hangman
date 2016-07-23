22010e57153a84f8a619d5603995ac1f9c56aa5a
Andrew Murriel <murriela1@gmail.com>
2016-07-22 23:30:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 var Letter = require('./letter');
 var Word = require('./word');
 var Game = require('./game');
+
+word.start();
+
 //var tWord = new Word('apple');
 // tWord.testWord();
-//tWord.convertString();
-/*console.log('preguess');
+/*tWord.convertString();
+console.log('preguess');
 console.log(tWord.displayWord());
 tWord.checkLetter('p');
 console.log('postguess');
 
 console.log(tWord.displayWord());*/
-word.start();
